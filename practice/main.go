025e4278d5b91f229428bc31be3bb2271a5987e2
orange-na <otuy1125abman@gmail.com>
2024-08-20 19:05:56 +0900

    📝

--- practice/main.go
+++ practice/main.go
@@ -1,13 +1,24 @@
-package main // メインプログラムは必ずこうする
-import (     // 読み込むパッケージ(ライブラリ)の宣言
-	"fmt" // 入出力関連のパッケージ 。Scanln と Printlnを使う "strconv" // 文字列から整数などへの変換。Atoiを使う
-) // 複数のパッケージを呼び込む場合は、(...)で指定すると「import」は1個で済む
+package main
+
+import (
+	"fmt"
+)
+
 func main() {
-	var s int
-	if s == 0 {
-		s = 1_4_4
-		fmt.Printf("s is %d\n", s)
-	} else {
-		fmt.Println("s is not empty")
+
+
+	type person struct {
+		name string 
+		age int
+		pet string
 	}
+
+	bob := person{
+		name: "Bob",
+		age: 25,
+		pet: "cat",
+	}
+
+	fmt.Println(bob.name)
+
 }
