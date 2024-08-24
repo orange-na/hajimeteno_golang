157865c50126701be219a3f69bb4e865193da205
orange-na <otuy1125abman@gmail.com>
2024-08-24 13:49:25 +0900

    ファイルを1行ずつ読み込む

--- practice/main.go
+++ practice/main.go
@@ -1,23 +1,41 @@
+// 1行ずつ正規表現を使って処理
 package main
 
 import (
-	"fmt" // 標準出力
-	"log" // ログを書くのに便利。
-	"os"  // ファイルの読み込みなど "strings" // 文字列置換
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
 	"strings"
 )
 
-// 定数の宣言(詳細は「2.3 定数」)。型は右辺のデフォルト(この場合string)になる
-const path1 = "./test.txt"
+var re1 = regexp.MustCompile(`[,，、] *`) // 正規表現をコンパイルしておく
+
 func main(){
-allData, err := os.ReadFile(path1) // 全データを一度に読み込み
-// allDataは[]uint8(8ビット符号なし整数のスライス)
-// io/ioutilにもReadFileがある。現在も動作するが、今は単に上のos.ReadFileが呼ばれる 
-if err != nil {
-	log.Fatal(err) // エラーをログに書いて異常終了 
+	data, _ := os.Open("./test.txt")
+	fmt.Println("data=", data)
+	defer data.Close()
+	
+	scanner := bufio.NewScanner(data)
+	fmt.Println("scanner.Scan()=", scanner.Scan())
+	for scanner.Scan() {
+		s := scanner.Text()
+		if s == "" || s[0:1] == "#" { // s[0:1] で先頭の1文字
+			continue  // この行はスキップ（コメントあるいは空白行）
+		}
+		columns := strings.Split(s, "|") // "|"で分割
+		if len(columns)!=2 { // カラム数が2個でないとき
+			continue // この行はスキップ。形式がおかしい
+		}
+		eng := columns[0] // 英語部分
+		jpn := columns[1] // 日本語訳
+		translations := re1.Split(jpn, -1) // 正規表現を使って分割。-1は全部
+		// []string（文字列のスライス）が戻る
+		for _, translation := range translations {
+			// range からはインデックス（添字）と文字列が戻るが、インデックスは無視する
+			// 「_」に代入すると無視できる
+			// 変数に代入するとその変数を使わないとエラーになってしまうので
+			fmt.Printf("%s|%s\n", eng, translation);
+		}
 	}
-s := string(allData) // string(allData)で型をstringに変換してから呼び出し 
-s = strings.ReplaceAll(s, "//", "##") // 「//」を「##」にすべて置換
-s = strings.ReplaceAll(s, "\t", "|") // 「タブ」を「|」にすべて置換 
-fmt.Printf("%s", s) // 文字列置換した結果を出力
 }
